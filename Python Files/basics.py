630265c351fc512120e4129210ef36868854c720
Colon3l-ops <tashcollins01@gmail.com>
2025-01-31 10:38:11 +0300

    python

--- Python Files/basics.py
+++ Python Files/basics.py
@@ -1,2 +1,16 @@
 # displaying text
 print ("Hello World")
+#variables
+# avariable that stores a value or a named container
+fname="Matthew"
+lname="James"
+age=16
+course="fullstack"
+#displaying Variables
+print(fname)
+print(lname)
+print(age)
+print(course)
+#displaying a string and a variable
+print("Hello my name is",fname)
+print("I am",age,"years old")
